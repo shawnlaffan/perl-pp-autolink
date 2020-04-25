4171e6633100c6ddf4805c646ad6dff71dfc2a75
Shawn Laffan <shawnlaffan@gmail.com>
2020-04-25 21:47:46 +1000

    even more feedback

--- bin/pp_autolink.pl
+++ bin/pp_autolink.pl
@@ -300,7 +300,7 @@ sub get_autolink_list_ldd {
               or $path =~ m{\Qdarwin-thread-multi-2level/auto/share/dist/Alien\E}  #  alien in share
               or $name =~ m{^lib(?:gcc_s|stdc\+\+)\.}  #  should already be packed
               ) {
-                warn "skipping $name";
+                warn "skipping $name => $path";
                 warn "re1" if $path =~ m{^(?:/usr)?/lib(?:32|64)?/};
                 warn "re2" if $path =~ m{\Qdarwin-thread-multi-2level/auto/share/dist/Alien\E};
                 warn "re3" if $name =~ m{^lib(?:gcc_s|stdc\+\+)\.};
