ba14d3614d107b3ba1577ca3eb4e2f365922f602
Shawn Laffan <shawnlaffan@gmail.com>
2020-04-25 14:16:41 +1000

    test script
    Should really be in t/

---
+++ tester.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+
+BEGIN {
+    if ($ENV{PAR_0}) {
+        print "Adding $ENV{PAR_TEMP} to the path\n";
+        use Config;
+        $ENV{PATH} = "$ENV{PAR_TEMP}$Config{path_sep}$ENV{PATH}";
+    }
+};
+
+use Gtk2;
+Gtk2->init;
+
+print "1";
