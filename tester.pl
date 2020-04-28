2fd82b1db5613e49cb32f828781db1a417d1084a
Shawn Laffan <shawnlaffan@gmail.com>
2020-04-28 17:32:11 +1000

    Refactor main code into a (traditional) OO package
    More cleanup is needed, but it runs on Windows
    
    Updates #10

--- tester.pl
+++ tester.pl
@@ -13,4 +13,10 @@ use Gtk2;
 use Alien::sqlite;
 
 print "1\n";
-print "Alien::sqlite install type: " . Alien::sqlite->install_type . "\n";
+print "Alien::sqlite install type: "
+     . Alien::sqlite->install_type
+     . "\n";
+print "Alien::sqlite dynamic libs: "
+    . (join ' ', Alien::sqlite->dynamic_libs)
+    . "\n";
+print "end of script $0\n";
