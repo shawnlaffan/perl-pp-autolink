2ae0b1279e43f930cbbc56c7b6fe16f297eaa2c4
Shawn Laffan <shawnlaffan@gmail.com>
2020-06-21 09:45:22 +1000

    Bump version to 2.04

--- lib/App/PP/Autolink.pm
+++ lib/App/PP/Autolink.pm
@@ -24,7 +24,7 @@ use Config;
 use Getopt::ArgvFile default=>1;
 use Getopt::Long qw / GetOptionsFromArray :config pass_through /;
 
-our $VERSION = '2.03';
+our $VERSION = '2.04';
 
 use constant CASE_INSENSITIVE_OS => ($^O eq 'MSWin32');
 
